1459d83eb7bbd0b65524b78cc3d4ba9752d95623
AlexAlarcon <125610346+HALEXALARCON@users.noreply.github.com.>
2025-04-24 21:25:29 -0500

    PokeMons

--- src/routes/Protected.jsx
+++ src/routes/Protected.jsx
@@ -2,13 +2,16 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useName } from "../contexts/NameContext";
 
 function Protected({ children }) {
-  const [state] = useName();
+  // Desestructuramos directamente el nombre del estado
+  const [{ name }] = useName();
 
-  if (!state.name) {
-    return <Navigate to='/' />;
+  // Si no hay nombre, redirigimos al Home reemplazando la entrada en el historial
+  if (!name) {
+    return <Navigate to="/" replace />;
   }
 
-  return children ? children : <Outlet />;
+  // Renderizamos los children si vienen, o el <Outlet /> para rutas anidadas
+  return children || <Outlet />;
 }
 
 export default Protected;
